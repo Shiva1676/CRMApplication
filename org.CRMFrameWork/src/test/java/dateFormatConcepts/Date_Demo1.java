73bc58828264affe5fdd315efa4cf1f1aa83b6fc
Lenovo <Lenovo@192.168.0.131>
2019-09-22 22:47:25 +0530

    Changes

--- org.CRMFrameWork/src/test/java/dateFormatConcepts/Date_Demo1.java
+++ org.CRMFrameWork/src/test/java/dateFormatConcepts/Date_Demo1.java
@@ -9,7 +9,7 @@ public class Date_Demo1 {
 	@Test
 	public void test(){
 		
-		LocalDate date = LocalDate.now().plusDays(1);
+		LocalDate date = LocalDate.now().plusDays(2);
 		
 		System.out.println(date);
 	}
