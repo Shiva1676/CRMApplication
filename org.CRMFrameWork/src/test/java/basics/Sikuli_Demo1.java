0527e482e7794b4a49ed041becccfc2b04c8527a
Lenovo <Lenovo@192.168.0.131>
2019-09-23 08:32:37 +0530

    Updated

---
+++ org.CRMFrameWork/src/test/java/basics/Sikuli_Demo1.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package basics;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
+import org.testng.annotations.Test;
+
+/**
+ * @author Lenovo
+ *
+ */
+public class Sikuli_Demo1 {
+
+	@Test
+	public void test() throws FindFailed, InterruptedException, AWTException {
+
+		// Thread.sleep(2000);
+
+		Robot r = new Robot();
+
+		r.keyPress(KeyEvent.VK_WINDOWS);
+		r.keyPress(KeyEvent.VK_D);
+
+		r.keyRelease(KeyEvent.VK_WINDOWS);
+		r.keyRelease(KeyEvent.VK_D);
+
+		Screen s = new Screen();
+
+		s.doubleClick("C:\\Users\\Lenovo\\git\\CRMApplication\\org.CRMFrameWork\\SikuliFolder\\Capture.PNG");
+
+	}
+
+}
